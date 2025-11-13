075473a74b8bcf465cdb2697aa4a645d1e98de6f
marlevek <marcelo_levek@hotmail.com>
2025-11-13 14:27:42 -0300

    reorganizado o código

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
 <footer class="footer">
-    <div class="container">
-        <a href="https://www.instagram.com/codertec.ia" class="instagram" rel="noopener noreferrer" target="_blank"><i class="bi bi-instagram"> </i> Codertec.ia</a>
+    <div class="footer-container">
+        <div class="social-links">
+            <a href="https://www.instagram.com/codertec.ia" class="instagram" rel="noopener noreferrer" target="_blank"><i class="bi bi-instagram"> </i> Codertec.ia</a>
+            <span class="brand-separator">|</span>
+            <a href="https://www.linkedin.com/in/marlevek" class="linkedin" target="_blank" rel="noopener noreferrer"><i class="bi-linkedin"></i> marlevek</a>
+
+        </div>
         <p class="pt-4">&copy; <?php echo date('Y'); ?> CoderTec. Todos os direitos reservados.</p>
     </div>
 </footer>
@@ -15,4 +20,5 @@
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
